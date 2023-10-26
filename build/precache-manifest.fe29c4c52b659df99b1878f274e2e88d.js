2de6969b1d87d450255abdf1ae64329f68685a81
dnisah <dnishad158@gmail.com>
2023-10-26 22:39:44 +0530

    initial commit

---
+++ build/precache-manifest.fe29c4c52b659df99b1878f274e2e88d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee21ca3faa86caffa641",
+    "url": "/static/css/main.3b300f60.chunk.css"
+  },
+  {
+    "revision": "ee21ca3faa86caffa641",
+    "url": "/static/js/main.07eb2889.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "58ef68fcd39efcc3e691",
+    "url": "/static/css/2.27d61378.chunk.css"
+  },
+  {
+    "revision": "58ef68fcd39efcc3e691",
+    "url": "/static/js/2.360a3732.chunk.js"
+  },
+  {
+    "revision": "b56d2ee6c65e0104e4ef76c027587e46",
+    "url": "/static/media/tabler-webfont.b56d2ee6.woff2"
+  },
+  {
+    "revision": "924bfe321562eb8cdedf675ad0e011b3",
+    "url": "/static/media/tabler-webfont.924bfe32.woff"
+  },
+  {
+    "revision": "f24b133ed4406bb17876c3ed7fa86f22",
+    "url": "/static/media/tabler-webfont.f24b133e.eot"
+  },
+  {
+    "revision": "68520a6b16e40c7b3538bae7c43eb776",
+    "url": "/static/media/tabler-webfont.68520a6b.ttf"
+  },
+  {
+    "revision": "ad55e63e508f5b0e55e9d2394a2732c7",
+    "url": "/static/media/tabler-webfont.ad55e63e.svg"
+  },
+  {
+    "revision": "0325d737308ae7a925b9a0195dd482e0",
+    "url": "/index.html"
+  }
+];
